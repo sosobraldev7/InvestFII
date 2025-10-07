9bb1e1a46fd64541aaca0fdd094ce9c595da373e
sosobraldev7 <sosobral.tech@gmail.com>
2025-10-07 19:27:32 -0300

    Site ajustado V2.2

--- sever/sever.py
+++ sever/sever.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, request, jsonify # type: ignore
+from flask_cors import CORS # type: ignore
 import os
 import openai # type: ignore
 
